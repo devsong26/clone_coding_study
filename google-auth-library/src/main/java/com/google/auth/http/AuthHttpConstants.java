d7fdaee13004c2d1e4c6afc09072844d6b287437
devsong26 <devsong26@gmail.com>
2022-01-18 23:23:24 +0900

    google-auth-library cloning 4days

---
+++ google-auth-library/src/main/java/com/google/auth/http/AuthHttpConstants.java
@@ -0,0 +1,9 @@
+package com.google.auth.http;
+
+public class AuthHttpConstants {
+
+    public static final String BEARER = "Bearer";
+
+    public static final String AUTHORIZATION = "Authorization";
+
+}
---
+++ google-auth-library/src/main/java/com/google/auth/http/InternalAuthHttpConstants.java
@@ -0,0 +1,7 @@
+package com.google.auth.http;
+
+class InternalAuthHttpConstants {
+
+    static final String BEARER_PREFIX = AuthHttpConstants.BEARER + " ";
+
+}
