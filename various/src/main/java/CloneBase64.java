880f8d51f4a29137d282cdd81518134a9d58f945
devsong26 <devsong26@gmail.com>
2022-02-28 23:00:30 +0900

    Base64 - 5

--- various/src/main/java/CloneBase64.java
+++ various/src/main/java/CloneBase64.java
@@ -531,4 +531,145 @@ public class CloneBase64 {
 
     }
 
+    private static class CloneDecInputStream extends InputStream {
+
+        private final InputStream is;
+        private final boolean isMIME;
+        private final int[] base64;     // base64 -> byte mapping
+        private int bits = 0;           // 24-bit buffer for decoding
+        private int nextin = 18;        // next available "off" in "bits" for input;
+                                        // -> 18, 12, 6, 0
+        private int nextout = -8;       // next available "off" in "bits" for output;
+                                        // -> 8, 0, -8 (no byte for output)
+        private boolean eof = false;
+        private boolean closed = false;
+
+        CloneDecInputStream(InputStream is, int[] base64, boolean isMIME){
+            this.is = is;
+            this.base64 = base64;
+            this.isMIME = isMIME;
+        }
+
+        private byte[] sbBuf = new byte[1];
+
+        @Override
+        public int read() throws IOException {
+            return read(sbBuf, 0, 1) == -1 ? -1 : sbBuf[0] & 0xff;
+        }
+
+        @Override
+        public int read(byte[] b, int off, int len) throws IOException {
+            if(closed){
+                throw new IOException("Stream is closed");
+            }
+            if(eof && nextout < 0){     // eof and no leftover
+                return -1;
+            }
+            if(off < 0 || len < 0 || len > b.length - off){
+                throw new IndexOutOfBoundsException();
+            }
+
+            int oldOff = off;
+            if(nextout >= 0){
+                do {
+                    if(len == 0){
+                        return off - oldOff;
+                    }
+                    b[off++] = (byte)(bits >> nextout);
+                    len--;
+                    nextout -= 8;
+                } while(nextout >= 8);
+                bits = 0;
+            }
+            while(len > 0){
+                int v = is.read();
+                if(v == -1){
+                    eof = true;
+                    if (nextin != 18){
+                        if(nextin == 12){
+                            throw new IOException("Base64 stream has one un-decoded dangling byte.");
+                        }
+                        //treat ending xx/xx without padding character legal.
+                        //same logic as v == '=' below
+                        b[off++] = (byte)(bits >> (16));
+                        len--;
+                        if(nextin == 0){            // only one padding byte
+                            if(len == 0){           // no enough output space
+                                bits >>= 8;         // shift to lowest byte
+                                nextout = 0;
+                            }else{
+                                b[off++] = (byte)(bits >> 8);
+                            }
+                        }
+                    }
+                    if(off == oldOff){
+                        return -1;
+                    }else{
+                        return off - oldOff;
+                    }
+                }
+                if(v == '='){                   // padding byte(s)
+                    // =     shiftto==18 unnecessary padding
+                    // x=    shiftto==12 dangling x, invalid unit
+                    // xx=   shiftto==6 && missing last '='
+                    // xx=y  or last is not '='
+                    if(nextin == 18 || nextin == 12 ||
+                        nextin == 6 && is.read() != '='){
+                        throw new IOException("Illegal base64 ending sequence:" + nextin);
+                    }
+                    b[off++] = (byte)(bits >> (16));
+                    len--;
+                    if(nextin == 0){        //only one padding byte
+                        if(len == 0){       // no enough output space
+                            bits >>= 8;     // shift to lowest byte
+                            nextout = 0;
+                        }else{
+                            b[off++] = (byte)(bits >> 8);
+                        }
+                    }
+                    eof = true;
+                    break;
+                }
+                if((v = base64[v]) == -1){
+                    if(isMIME){
+                        continue;
+                    }else
+                        throw new IOException("Illegal base64 character " + Integer.toString(v, 16));
+                }
+
+                bits |= (v << nextin);
+                if(nextin == 0){
+                    nextin = 18;    //clear for next
+                    nextout = 16;
+                    while(nextout >= 0){
+                        b[off++] = (byte)(bits >> nextout);
+                        len--;
+                        nextout -= 8;
+                        if(len ==0 && nextout >= 0) // don't clear "bits"
+                            return off - oldOff;
+                    }
+                    bits = 0;
+                }else{
+                    nextin -= 6;
+                }
+            }
+            return off - oldOff;
+        }
+
+        @Override
+        public int available() throws IOException{
+            if(closed)
+                throw new IOException("Stream is closed");
+            return is.available();  // TBD;
+        }
+
+        @Override
+        public void close() throws IOException{
+            if(!closed){
+                closed = true;
+                is.close();
+            }
+        }
+    }
+
 }
